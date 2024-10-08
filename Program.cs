f5036ae013df079c73b094dc081537540a8d7241
whitney-wolf-student <whwo336@student.oaisd.org>
2024-10-08 13:15:06 +0000

    Finished program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Whitney Wolf.");
+
+            //Using Variables
+            int age = 16;
+            string greeting = "Hello World";
+            string firstname = "Whitney";
+            string lastname = "Wolf";
+            double money = 2.79;
+
+            Console.WriteLine (greeting + "! " + "My name is " + firstname + " "
+            + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+        }
+}
